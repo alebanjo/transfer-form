5c85cc56f05b1f73e7936c0d20fd802fa13d01b5
= <=>
2017-10-29 00:02:27 -0400

    removed front-end

--- server.js
+++ server.js
@@ -73,10 +73,6 @@ app.post('/api/newclient', function (req, res, next) {
 	});
 });
 
-app.get('/', function(req,res){
-	res.sendFile(__dirname + '/public/index.html');
-});
-
 app.get('*', function(req,res){
 	res.end('404');
 });
